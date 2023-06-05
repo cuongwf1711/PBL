c1187cb0c6b33269c38d3d1784be26ae58335822
cuongwf1711 <128906983+cuongwf1711@users.noreply.github.com>
2023-06-05 12:57:25 +0700

    Add files via upload
    DTO

---
+++ Account.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PBL.DTO
+{
+    public class Account : Person
+    {
+        [Index(IsUnique = true), EmailAddress, StringLength(100), Required]
+        public string Email { get; set; }
+        [StringLength(64), Required]
+        public string Password { get; set; }
+        public int RoleId { get; set; }
+        public virtual Role Role { get; set; }
+
+        public virtual ICollection<Payment> Payments { get; set; }
+    }
+}
